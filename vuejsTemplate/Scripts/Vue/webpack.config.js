66d1227b2e023208da84eee283f5a74959851db0
Jose M Ramirez <josemramirez81@outlook.com>
2019-02-19 00:44:55 -0500

    First Commit: Webpack config, and Vue setup

---
+++ vuejsTemplate/Scripts/Vue/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+      path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader' },
+            { test: /\.vue$/, use: 'vue-loader' },
+            { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new VueLoaderPlugin(),
+    ]
+};
